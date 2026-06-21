7b506d7ceb966a3299e5680c3c3907f222ba13d8
Guillermo Rayas M <bodisatva5@outlook.com>
2026-06-20 23:48:08 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ enterBtn.addEventListener("click", () => {
 welcomeScreen.addEventListener("click", () => {
   enterBtn.click();
 });
+
+    // Grab your model viewer element
+    const viewer = document.querySelector("model-viewer");
+    
+    // Explicitly enforce the neutral lighting setup via JS API
+    viewer.addEventListener("load", () => {
+        viewer.environmentImage = "neutral";
+    });
