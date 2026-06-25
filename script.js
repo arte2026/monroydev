b5703f3f6acc7fcdde95c0ecfea9fea80252d074
Guillermo Rayas M <bodisatva5@outlook.com>
2026-06-24 23:59:55 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-//WELCOME
-const welcomeScreen = document.getElementById("welcomeScreen");
-const enterBtn = document.getElementById("enterBtn");
 
-enterBtn.addEventListener("click", () => {
-  welcomeScreen.classList.add("fade-out");
-
-  setTimeout(() => {
-    welcomeScreen.style.display = "none";
-  }, 900);
-
-});
-welcomeScreen.addEventListener("click", () => {
-  enterBtn.click();
-});
-
+// Handles loading the events for <model-viewer>'s slotted progress bar
+const onProgress = (event) => {
+  const progressBar = event.target.querySelector('.progress-bar');
+  const updatingBar = event.target.querySelector('.update-bar');
+  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+  if (event.detail.totalProgress === 1) {
+    progressBar.classList.add('hide');
+    event.target.removeEventListener('progress', onProgress);
+  } else {
+    progressBar.classList.remove('hide');
+  }
+};
+document.querySelector('model-viewer').addEventListener('progress', onProgress);
