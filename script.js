90ef6eb55bac77fc743aee023f482d113ad423a3
Guillermo Rayas M <bodisatva5@outlook.com>
2026-06-27 20:50:34 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-
-// Handles loading the events for <model-viewer>'s slotted progress bar
-const onProgress = (event) => {
-  const progressBar = event.target.querySelector('.progress-bar');
-  const updatingBar = event.target.querySelector('.update-bar');
-  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
-  if (event.detail.totalProgress === 1) {
-    progressBar.classList.add('hide');
-    event.target.removeEventListener('progress', onProgress);
-  } else {
-    progressBar.classList.remove('hide');
-  }
-};
-document.querySelector('model-viewer').addEventListener('progress', onProgress);
-
 const isMobile =
   /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(navigator.userAgent)
   || window.innerWidth <= 640;
@@ -20,3 +5,27 @@ const isMobile =
 if (isMobile) {
   window.location.href = "mobile/index.html";
 }
+
+// Handles loading the events for <model-viewer>'s slotted progress bar
+const onProgress = (event) => {
+  const progressBar = event.target.querySelector('.progress-bar');
+  const updatingBar = event.target.querySelector('.update-bar');
+  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+  if (event.detail.totalProgress === 1) {
+    event.target.addEventListener(
+        'load',
+        () => {
+            requestAnimationFrame(() => {
+                progressBar.classList.add('hide');
+            });
+        },
+        { once: true }
+    );
+
+    event.target.removeEventListener('progress', onProgress);
+}
+    else {
+    progressBar.classList.remove('hide');
+  }
+};
+document.querySelector('model-viewer').addEventListener('progress', onProgress);
